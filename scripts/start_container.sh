f082246ed9fe218b697924d4234a357d7a924955
Jayalakshmi <141424247+Jayalakshmi-i@users.noreply.github.com>
2024-06-20 19:16:21 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull jayalakshmii/simple-python-app:latest
+docker pull jayalakshmii/simple-python-ci-flask-app:latest
 # docker pull abhishekf5/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 jayalakshmii/simple-python-app:latest
+docker run -d -p 5000:5000 jayalakshmii/simple-python-ci-flask-app:latest
 # docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
